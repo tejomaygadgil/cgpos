122f4dcf990d2be449323060f815f154ee05100f
tejomaygadgil <tejomaygadgil@gmail.com>
2023-08-10 16:13:03 -0400

    Add method to read XML Perseus data (!)

--- src/cgpos/data/make_dataset.py
+++ src/cgpos/data/make_dataset.py
@@ -1,30 +1,61 @@
 # -*- coding: utf-8 -*-
-import click
 import logging
-from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+import os
+import pickle
+import xml.etree.ElementTree as ET
+
+import hydra
+from omegaconf import DictConfig
+
+from cgpos.utils.util import get_abs_dir
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
+@hydra.main(config_path="../../../conf", config_name="main", version_base=None)
+def read_perseus(config: DictConfig):
+    """
+    Converts raw Perseus treebank XML data into a tabular format.
     """
     logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+    logger.info("Processing Perseus data:")
+
+    # Set import and export directories
+    abs_dir = get_abs_dir()
+    import_dir = os.path.join(abs_dir, config.data_dir.perseus)
+    export_dir = os.path.join(abs_dir, config.data_dir.processed, "perseus.pkl")
+
+    # Get files
+    files = os.listdir(import_dir)
+    logger.info(f"Importing {len(files)} files from {import_dir}")
+
+    # Collect words from files
+    data = []
+    for file in files:
+        file_dir = os.path.join(import_dir, file)
+        logger.info(f"Processing {file}")
+
+        # Load XML
+        tree = ET.parse(file_dir)
+        root = tree.getroot()
+
+        # Iterate through sentences
+        for sentence in root.iter("sentence"):
+            sentence_attrib = sentence.attrib
+            sentence_attrib["sentence_id"] = sentence_attrib.pop("id")  # Rename
+            for word in sentence:
+                word_attrib = word.attrib
+                word_attrib.update(sentence_attrib)
+                data.append(word_attrib)
+
+    logging.info(f"Success! Extracted {len(data)} words from {len(files)} files.")
+
+    # Export as pickle
+    logging.info(f"Exporting to {export_dir}")
+    with open(export_dir, "wb") as file:
+        pickle.dump(data, file)
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+if __name__ == "__main__":
+    log_fmt = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=log_fmt)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
-    main()
+    read_perseus()
