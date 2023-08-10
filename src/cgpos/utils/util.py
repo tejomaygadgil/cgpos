b96e0baefebd9a4e5fd1e642def04f42e5c003f7
tejomaygadgil <tejomaygadgil@gmail.com>
2023-08-10 16:34:19 -0400

    Simplify get_abs_dir and add load_perseus method

--- src/cgpos/utils/util.py
+++ src/cgpos/utils/util.py
@@ -1,8 +1,10 @@
+import os
 from pathlib import Path
 
 
-def get_abs_dir(file=__file__):
+def get_abs_dir(path):
     """
     Helper function to return the absolute path from within a package (data, features, etc.)
     """
-    return Path(file).resolve().parents[3]
+    abs_path = os.path.join(Path(__file__).resolve().parents[3], path)
+    return abs_path
