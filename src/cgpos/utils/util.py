cab775bede81b152007fc99cf78a8f94e3e246c4
tejomaygadgil <tejomaygadgil@gmail.com>
2023-08-11 18:16:50 -0400

    Add `greek-accentuation` package to poetry + greek normalization constants to util.py

--- src/cgpos/utils/util.py
+++ src/cgpos/utils/util.py
@@ -1,6 +1,14 @@
 import os
 from pathlib import Path
 
+from greek_accentuation.characters import (
+    Accent,
+    Breathing,
+    Diacritic,
+    Length,
+    Subscript,
+)
+
 
 def get_abs_dir(path):
     """
@@ -8,3 +16,29 @@ def get_abs_dir(path):
     """
     abs_path = os.path.join(Path(__file__).resolve().parents[3], path)
     return abs_path
+
+
+# Some sets of Unicode Greek characters. Useful for normalization
+GREEK_LOWER = set(range(0x03B1, 0x03CA))
+GREEK_UPPER = set(range(0x0391, 0x03AA))
+GREEK_CHARACTERS = set.union(GREEK_LOWER, GREEK_UPPER)
+GREEK_DIACRITICS = {  #  Data from `greek_accentuation` library
+    ord(mark.value)
+    for mark_set in [
+        Breathing,
+        Accent,
+        Diacritic,
+        Subscript,
+        Length,
+    ]
+    for mark in mark_set
+    if (type(mark.value) is not int)  # Breathing contains -1 value for some reason
+}
+GREEK_PUNCTUATION = {  # cf. https://www.degruyter.com/document/doi/10.1515/9783110599572-009/html p. 153
+    0x002C,  # Comma
+    0x002E,  # Full stop
+    0x003B,  # Semicolon (question)
+    0x00B7,  # Middle dot (semicolon)
+    # 0x02B9, # Modifier letter prime (?) -- Not in use
+    0x2019,  # Right single quotation mark (elision)
+}
