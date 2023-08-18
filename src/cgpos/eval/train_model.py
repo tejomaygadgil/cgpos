590cbed0b6daea7e69eb29f2f992253fb9617bee
tejomaygadgil <tejomaygadgil@gmail.com>
2023-08-18 16:30:51 -0400

    Incorporate all models into model eval and train

--- src/cgpos/eval/train_model.py
+++ src/cgpos/eval/train_model.py
@@ -42,35 +42,18 @@ def train_model(config: DictConfig):
     runs_dir = get_abs_dir(config.runs_dir)
     timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
     run_dir = os.path.join(runs_dir, timestamp)
-    os.makedirs(run_dir)
+    param_grid_dir = os.path.join(run_dir, "param_grid")
+    os.makedirs(param_grid_dir)
 
     # Set data
     X = features
     y = np.array(targets)
-
-    # Import clf
-    clf_name = config.train.clf
-    clf_module = import_module(config.train.clf_module)
-    clf = getattr(clf_module, clf_name)
-    logger.info(f"Training {clf_name}:")
-
-    # Export clf name
-    clf_name_dir = os.path.join(run_dir, "clf_name.pkl")
-    export_pkl(clf_name, clf_name_dir)
 
     # Get CV args
     test_split_args = config.train.test_split
     tune_split_args = config.train.tune_split
     f1_average = config.train.f1_average
     export_pred = config.train.export_pred
-
-    # Set up parameter grid
-    clf_param = config.param_grid[clf_name]
-    param_grid = get_clf_args(clf_param)
-
-    # Export parameter grid
-    param_grid_dir = os.path.join(run_dir, "param_grid.pkl")
-    export_pkl(param_grid, param_grid_dir)
 
     # test CV loop
     test_splitter = ShuffleSplit(**test_split_args)
@@ -105,55 +88,75 @@ def train_model(config: DictConfig):
         export_pkl(X_test, X_train_export_dir, verbose=False)
         export_pkl(y_test, y_train_export_dir, verbose=False)
 
-        # Loop through targets
-        targets_len = len(targets_name)
-        for target in range(targets_len):
-            target_name = targets_name[target]
-            logger.info(f"Target {target + 1} of {targets_len} ({target_name}):")
+        # Loop through models
+        clfs_name = config.train.clfs
+        clfs_len = len(clfs_name)
+        for clf in range(clfs_len):
+            # Import clf
+            clf_name = clfs_name[clf]
+            clf_module = import_module(config.train.clf_module)
+            clf_method = getattr(clf_module, clf_name)
+            logger.info(f"Training classifier {clf + 1} of {clfs_len} ({clf_name}):")
 
-            # Hyperparameter tuning loop
-            _y_i_temp = _y_temp[:, target]
-            tune_splitter = StratifiedShuffleSplit(**tune_split_args)
-            defaultdict(lambda: defaultdict(list))
-            dummy_X = [0] * len(_y_i_temp)
-            tune_splits = tune_splitter.split(dummy_X, _y_i_temp)
-            for tune, (train_indices, dev_indices) in enumerate(tune_splits):
-                logger.info(f"Tune split {tune + 1} of {tune_split_args['n_splits']}:")
+            # Set up parameter grid
+            clf_param = config.param_grid[clf_name]
+            clf_param_grid = get_clf_args(clf_param)
 
-                # Make train and dev sets
-                X_i_train = [_X_temp[index] for index in train_indices]
-                y_i_train = _y_temp[:, target][train_indices]
+            # Export parameter grid
+            clf_param_grid_dir = os.path.join(param_grid_dir, f"{clf_name}.pkl")
+            export_pkl(clf_param_grid, clf_param_grid_dir)
 
-                X_i_dev = [_X_temp[index] for index in dev_indices]
-                y_i_dev = _y_i_temp[dev_indices]
+            # Loop through targets
+            targets_len = len(targets_name)
+            for target in range(targets_len):
+                target_name = targets_name[target]
+                logger.info(f"Target {target + 1} of {targets_len} ({target_name}):")
 
-                # Set run parameters
-                file_stem = f"target{target}_tune{tune}_clfarg"
-                score_dir_stem = os.path.join(scores_dir, file_stem)
-                pred_dir_stem = os.path.join(preds_dir, file_stem)
-                run_clf_arg = {
-                    "clf": clf,
-                    "f1_score": f1_score,
-                    "X_i_train": X_i_train,
-                    "y_i_train": y_i_train,
-                    "X_i_dev": X_i_dev,
-                    "y_i_dev": y_i_dev,
-                    "f1_average": f1_average,
-                    "score_dir_stem": score_dir_stem,
-                    "pred_dir_stem": pred_dir_stem,
-                    "export_pred": export_pred,
-                }
+                # Hyperparameter tuning loop
+                _y_i_temp = _y_temp[:, target]
+                tune_splitter = StratifiedShuffleSplit(**tune_split_args)
+                defaultdict(lambda: defaultdict(list))
+                dummy_X = [0] * len(_y_i_temp)
+                tune_splits = tune_splitter.split(dummy_X, _y_i_temp)
+                for tune, (train_indices, dev_indices) in enumerate(tune_splits):
+                    logger.info(
+                        f"Tune split {tune + 1} of {tune_split_args['n_splits']}:"
+                    )
 
-                # Parallelize model runs
-                with ProcessPoolExecutor() as executor:
-                    futures = []
-                    # Loop through parameter grid
-                    for i, clf_arg in enumerate(param_grid):
-                        future = executor.submit(run_clf, i, clf_arg, run_clf_arg)
-                        futures.append(future)
+                    # Make train and dev sets
+                    X_i_train = [_X_temp[index] for index in train_indices]
+                    y_i_train = _y_temp[:, target][train_indices]
 
-                    for future in tqdm(futures, total=len(param_grid)):
-                        future.result()
+                    X_i_dev = [_X_temp[index] for index in dev_indices]
+                    y_i_dev = _y_i_temp[dev_indices]
+
+                    # Set run parameters
+                    file_stem = f"clf{clf}_target{target}_tune{tune}_clfarg"
+                    score_dir_stem = os.path.join(scores_dir, file_stem)
+                    pred_dir_stem = os.path.join(preds_dir, file_stem)
+                    run_clf_arg = {
+                        "clf_method": clf_method,
+                        "f1_score": f1_score,
+                        "X_i_train": X_i_train,
+                        "y_i_train": y_i_train,
+                        "X_i_dev": X_i_dev,
+                        "y_i_dev": y_i_dev,
+                        "f1_average": f1_average,
+                        "score_dir_stem": score_dir_stem,
+                        "pred_dir_stem": pred_dir_stem,
+                        "export_pred": export_pred,
+                    }
+
+                    # Parallelize model runs
+                    with ProcessPoolExecutor() as executor:
+                        futures = []
+                        # Loop through parameter grid
+                        for i, clf_arg in enumerate(clf_param_grid):
+                            future = executor.submit(run_clf, i, clf_arg, run_clf_arg)
+                            futures.append(future)
+
+                        for future in tqdm(futures, total=len(clf_param_grid)):
+                            future.result()
 
 
 if __name__ == "__main__":
