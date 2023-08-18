71c688743babb67483a84b9ca2d58bb93968657b
tejomaygadgil <tejomaygadgil@gmail.com>
2023-08-18 01:53:37 -0400

    Fix naming

--- src/cgpos/features/build_features.py
+++ src/cgpos/features/build_features.py
@@ -23,34 +23,34 @@ def tokenize(config: DictConfig):
     # Import directories
     import_dir = config.data.cleaned
     # Export directories
-    feature_map_dir = config.reference.feature_map
+    features_map_dir = config.reference.features_map
     features_dir = config.data.features
 
     # Import data
     _, data, _ = import_pkl(import_dir)
 
     # Build features
-    feature_map = {}
+    features_map = {}
     features = []
     feature_token = 0
     for syllables in data:
         feature = []
         for syllable in syllables:
-            if syllable not in feature_map:
-                feature_map[syllable] = feature_token
+            if syllable not in features_map:
+                features_map[syllable] = feature_token
                 feature_token += 1
-            token = feature_map[syllable]
+            token = features_map[syllable]
             feature.append(token)
         features.append(feature)
 
     assert len(features) == len(data), "Input and output lengths do not match."
 
     # Export
-    export_pkl(feature_map, feature_map_dir)
+    export_pkl(features_map, features_map_dir)
     export_pkl(features, features_dir)
 
     logger.info(
-        f"Success! Extracted {len(feature_map)} tokens from {len(features)} words."
+        f"Success! Extracted {len(features_map)} tokens from {len(features)} words."
     )
 
 
