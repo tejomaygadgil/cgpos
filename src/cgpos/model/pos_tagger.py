8b3d8ba71251d46e5ef506a46fdb647fd1dbdf54
tejomaygadgil <tejomaygadgil@gmail.com>
2023-08-18 15:45:54 -0400

    Add PartOfSpeechTagger class

---
+++ src/cgpos/model/pos_tagger.py
@@ -0,0 +1,23 @@
+"""
+Ths module implements Multinomial Naive Bayes for part-of-speech tagging.
+"""
+
+# Author: Tejomay Gadgil <tejomaygadgil@gmail.com>
+
+
+class PartOfSpeechTagger:
+    """
+    Predicts fine-grained part-of-speech.
+    """
+
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+        # name = "Multinomial Naive Bayes"
+        # return f"{name} with alpha={self.alpha} and ngram_range={self.ngram_range}"
+
+    def predict(self, X: list) -> list:
+        preds = None
+        return preds
