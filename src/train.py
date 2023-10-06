d44317aaa3e6737207677983de37700d2b7f6640
tejomaygadgil <tejomaygadgil@gmail.com>
2023-10-05 20:17:23 -0700

    Increase complexity

--- src/train.py
+++ src/train.py
@@ -27,9 +27,9 @@ learning_rate = 1e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 eval_iters = 200
 generate_len = 32
-n_emb = 384
-n_head = 6
-n_layer = 6
+n_emb = 512
+n_head = 8
+n_layer = 8
 dropout = 0.3
 
 # Read, tokenize, and flatten text into one big list
