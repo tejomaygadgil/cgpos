403083b473fc54d4e13d1bdb0e97057c7046b1df
tejomaygadgil <tejomaygadgil@gmail.com>
2023-10-05 20:53:26 -0700

    Increase complexity

--- src/train.py
+++ src/train.py
@@ -20,15 +20,15 @@ logging.basicConfig(level=logging.DEBUG, format=log_fmt)
 
 # Hyperparameters
 batch_size = 64
-block_size = 256
+block_size = 512
 max_iters = 5000
 eval_interval = max_iters // 20
 learning_rate = 1e-4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 eval_iters = 200
 generate_len = 32
-n_emb = 512
 n_head = 8
+n_emb = n_head * 64
 n_layer = 8
 dropout = 0.3
 
