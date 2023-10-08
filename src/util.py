dd63b676068f76e3a0a5df76244f211dc51b07eb
tejomaygadgil <tejomaygadgil@gmail.com>
2023-10-08 09:45:06 -0700

    Bugfix

--- src/util.py
+++ src/util.py
@@ -32,7 +32,7 @@ def get_batch(data, block_size, batch_size, device, y=None):
     # Generate a small batch of data of inputs x and targets y
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i : i + block_size] for i in ix])
-    if y is None:
+    if y is not None:
         y = torch.stack([y[i + 1 : i + block_size + 1] for i in ix])
     else:
         y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
