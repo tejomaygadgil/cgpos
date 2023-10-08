379c79b9efb74fae6a4e6200fbf15387d58cb80b
tejomaygadgil <tejomaygadgil@gmail.com>
2023-10-08 08:24:53 -0700

    Add y param in get_batch

--- src/util.py
+++ src/util.py
@@ -28,11 +28,14 @@ def decode(itos, tokens):
 
 
 # Data loading
-def get_batch(data, block_size, batch_size, device):
+def get_batch(data, block_size, batch_size, device, y=None):
     # Generate a small batch of data of inputs x and targets y
     ix = torch.randint(len(data) - block_size, (batch_size,))
     x = torch.stack([data[i : i + block_size] for i in ix])
-    y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
+    if y:
+        y = torch.stack([y[i + 1 : i + block_size + 1] for i in ix])
+    else:
+        y = torch.stack([data[i + 1 : i + block_size + 1] for i in ix])
     x, y = x.to(device), y.to(device)
 
     return x, y
